c522e16e1dfa5c129d0b9419acf96887cc88ac2f
gusals005 <gusals005@gmail.com>
2021-07-29 13:11:07 +0000

    S1P-47 [feat]프로토타입에 쓰일 더미 데이터 입력

--- database/seeders/20210728174058-body_type.js
+++ database/seeders/20210728174058-body_type.js
@@ -3,8 +3,9 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('body_types', [{
+		id:1,
 		name: 'RightUp',
-		explanation : 'Right Up is not good.',
+		explanation : '오른쪽 골반이 조금 올라가 있습니다.',
 		pt_plan_id:1,
 		createdAt:new Date(),
 		updatedAt:new Date()
--- database/seeders/20210728174150-video.js
+++ database/seeders/20210728174150-video.js
@@ -2,13 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('videos', [{
-		name:'video first',
-		playtime:15.33,
-		explanation:'hi first video hi!',
-		createdAt:new Date(),
-        updatedAt:new Date()
-			}], {});
+    await queryInterface.bulkInsert('videos', [
+		{
+			id:1,
+			name:'hipbridge',
+			playtime:114,
+			explanation:'hipbridge video',
+			createdAt:new Date(),
+			updatedAt:new Date()
+		},
+		{
+			id:2,
+			name:'uparmleg',
+			playtime:99,
+			explanation:'uparmleg video',
+			createdAt:new Date(),
+			updatedAt:new Date()
+		},
+		{
+			id:3,
+			name:'tabata',
+			playtime:59,
+			explanation:'tabata video',
+			createdAt:new Date(),
+			updatedAt:new Date()
+		}
+	], {});
 		  /**
      * Add seed commands here.
      *
