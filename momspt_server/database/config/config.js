b8d29a2742230dad23c2c3e824a18d5b1da070c3
gusals005 <gusals005@gmail.com>
2021-08-02 07:38:22 +0000

    [chore]database/config 파일 변경

--- momspt_server/database/config/config.js
+++ momspt_server/database/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
 	}
 	*/
 	development :{
-		username : process.env.USERNAME,
+		username : process.env.USERID,
 		password : process.env.PASSWORD,
 		database : process.env.DATABASE,
 		host : process.env.HOST,
--- momspt_server/database/models/index.js
+++ momspt_server/database/models/index.js
@@ -15,6 +15,7 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
   console.log('use database');
+  console.log(config);
 }
 
 fs
