1b3825dc3799b1a2e2e9c39454dc97b93134d07e
LeeHyunmin <gusals005@gmail.com>
2021-09-16 15:41:10 +0900

    [docs]swagger 파일 업데이트

--- momspt_server/app.js
+++ momspt_server/app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //const specs = swaggerJsdoc(options);
 app.use("/api-docs",
   swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument)
+  swaggerUi.setup(swaggerDocument, {explorer: true})
 );
 
 
